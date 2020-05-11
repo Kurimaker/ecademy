ba26fe9d908e60100008107f426a4dbc73486913
lindh04 <47460872+lindh04@users.noreply.github.com>
2020-05-11 16:08:25 +0200

    easterEgg w/ code

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -9,6 +9,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String code = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,50 +32,93 @@ class _HomeState extends State<Home> {
               name: "Profil",
               color: Color.fromRGBO(113, 188, 192, 25),
               icon: "assets/images/home/profil.png",
-              onTap: () {Navigator.pushNamed(context, "/profile");},
-              onLongPress: () {Navigator.pushNamed(context, "/profile/easterEgg");},
+              onTap: () {
+                Navigator.pushNamed(context, "/profile");
+              },
+              onLongPress: () {
+                if (code == "1735") {
+                  Navigator.pushNamed(context, "/profile/easterEgg");
+                } else {
+                  code = "";
+                }
+              },
             ),
             LargeSquareButton(
               name: "Schema",
               color: Color.fromRGBO(232, 99, 99, 25),
               icon: "assets/images/home/schema.png",
-              onTap: () {Navigator.pushNamed(context, "/loading");},
+              onTap: () {
+                Navigator.pushNamed(context, "/loading");
+              },
+              onLongPress: () {
+                code = "${code}1";
+              },
             ),
             LargeSquareButton(
               name: "Skola",
               color: Color.fromRGBO(196, 220, 103, 25),
               icon: "assets/images/home/skola.png",
-              onTap: () {Navigator.pushNamed(context, "/loading");},
+              onTap: () {
+                Navigator.pushNamed(context, "/loading");
+              },
+              onLongPress: () {
+                  code = "${code}2";
+              },
             ),
             LargeSquareButton(
               name: "Meddelanden",
               color: Color.fromRGBO(128, 190, 106, 25),
               icon: "assets/images/home/meddelanden.png",
-              onTap: () {Navigator.pushNamed(context, "/loading");},
+              onTap: () {
+                Navigator.pushNamed(context, "/loading");
+              },
+              onLongPress: () {
+                code = "${code}3";
+              },
             ),
             LargeSquareButton(
               name: "Händelser",
               color: Color.fromRGBO(201, 105, 168, 25),
               icon: "assets/images/home/händelser.png",
-              onTap: () {Navigator.pushNamed(context, "/loading");},
+              onTap: () {
+                Navigator.pushNamed(context, "/loading");
+              },
+              onLongPress: () {
+                code = "${code}4";
+              },
             ),
             LargeSquareButton(
               name: "Kurser",
               color: Color.fromRGBO(217, 150, 89, 25),
               icon: "assets/images/home/kurser.png",
-              onTap: () {Navigator.pushNamed(context, "/courses");},
+              onTap: () {
+                Navigator.pushNamed(context, "/courses");
+              },
+              onLongPress: () {
+                code = "${code}5";
+              },
             ),
             LargeSquareButton(
               name: "Klass",
               color: Color.fromRGBO(251, 144, 144, 25),
               icon: "assets/images/home/klass.png",
-              onTap: () {Navigator.pushNamed(context, "/loading");},
+              onTap: () {
+                Navigator.pushNamed(context, "/loading");
+              },
+              onLongPress: () {
+                code = "${code}6";
+              },
             ),
             LargeSquareButton(
               name: "Sök",
               color: Color.fromRGBO(122, 120, 228, 25),
               icon: "assets/images/home/sök.png",
-              onTap: () {Navigator.pushNamed(context, "/loading");},
+              onTap: () {
+                Navigator.pushNamed(context, "/loading");
+              },
+              onLongPress: () {
+                code = "${code}7";
+              },
             ),
           ],
         ),
